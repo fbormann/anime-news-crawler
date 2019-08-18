8c0f11e4dca22cb81c6e7d4338c3f0b0de3e8c80
fbormann <felipe.bormann@gmail.com>
2019-08-18 19:51:10 -0300

    refactor(models/News.py): add default __init__ method without all fields

--- scrapper/models/News.py
+++ scrapper/models/News.py
@@ -5,9 +5,14 @@ from datetime import date
 @dataclass
 class News:
     title: str
+    portal_name: str
     url: str
     comment_amount: int
     word_count: int
     phrase_count: int
     paragraph_count: int
     publish_date: date
+
+    def __init__(self):
+        # because we don't necessarily want to have all fields initialized at the same time
+        pass
--- scrapper/spiders/animenews.py
+++ scrapper/spiders/animenews.py
@@ -1,21 +1,26 @@
 import scrapy
+import logging
 
 
 class AnimeNews(scrapy.Spider):
     name = 'animenews'
     start_urls = ["https://www.animenewsnetwork.com/news/"]
+    logger = logging.getLogger(__name__)
 
     # allowed_domains = ["animenewsnetwork.com"]
 
     def parse(self, response):
         main_feed = response.css('div.mainfeed-section')
 
+        amount_of_news_processed = 0
         for news_item in main_feed.css('div.news.herald'):
-            print(news_item.get())
+            self.logger.debug(news_item.get())
             comment_element = news_item.css('div.comments')
-            print(comment_element.get())
+            self.logger.debug(comment_element.get())
             title_element = news_item.css("div.wrap")
-            print(title_element.get())
-            print("title text: " + str(news_item.css("div.wrap h3:first-of-type > a::text").get()))
+            self.logger.debug(title_element.get())
+            self.logger.debug("title text: " + str(news_item.css("div.wrap h3:first-of-type > a::text").get()))
             article_link_element = news_item.css('div.thumbnail > a::attr(href)')
-            print(article_link_element.get())
+            self.logger.debug(article_link_element.get())
+            amount_of_news_processed += 1
+            self.logger.debug(amount_of_news_processed)
