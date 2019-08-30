a72e793ece5a770f7044dbae79b63054470a4aca
fbormann <felipe.bormann@gmail.com>
2019-08-30 04:37:09 -0300

    refactor(src/main.py): add command to run spider

--- scrapper/src/main.py
+++ scrapper/src/main.py
@@ -5,6 +5,6 @@ import os, sys
 if __name__ == '__main__':
     # THIS LINE IS ESSENTIAL TO SOLVE THE PROBLEM OF
     sys.path.append(os.getcwd())
-    process = CrawlerProcess()
-    process.crawl(AnimeNewsSpider)
-    process.start()
+    os.system("ls")
+    # TODO: parametize crawler between websites
+    os.system("scrapy runspider spiders/animenewsspider.py")
